865b6d45aa4661719235e848510858f5c09adfac
andressacarvalho-dev <andressalimadecarvalho4@gmail.com>
2025-12-05 09:53:20 -0300

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('index.html', views.post_list),
+    path('streaming.html', views.post_list),
+    path('metaverso.html', views.post_list),
+    path('museu.html', views.post_list),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
